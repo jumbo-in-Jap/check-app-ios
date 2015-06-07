f20ef3e4184c71bb188f90316afcaec7c944c138
gladenjoy <kirakutomoki@gladenjoynoMacBook.local>
2015-06-08 02:16:23 +0900

    add graph module

---
+++ check-out/DrawFigureMember.swift
@@ -0,0 +1,21 @@
+//
+//  DrawFigureMember.swift
+//  check-out
+//
+//  Created by 喜楽智規 on 2015/06/07.
+//  Copyright (c) 2015年 &#32701;&#30000; &#20581;&#22826;&#37070;. All rights reserved.
+//
+
+import UIKit
+
+class DrawFigureMember: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
